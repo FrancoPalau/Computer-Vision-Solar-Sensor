ba2144beee174951c91010b051b6414c6e334952
GonzaloFernandez <fernandez.gfg@gmail.com>
2019-08-17 17:29:17 -0300

    added limit switch setup

--- project/hardware.py
+++ project/hardware.py
@@ -3,7 +3,6 @@
 
     Autores:
     - Lautaro Delgado
-    - Jona
     - Franco Palau
     - Gonzalo Fernández
 
@@ -16,6 +15,41 @@
 import time
 import RPi.GPIO as GPIO
 
+
+def limitswitch_setup(ls_pin, pud, BOTH=False, int_both=None, RISING=False, int_rising=None, FALLING=False, int_falling=None):
+    """Setup of the limit switch
+
+    Parameters
+    ----------
+    ls_pin : int
+        The RPI's pin to connect to the limit switch.
+    pud : str
+        Parameter the specify resistor connection of the input
+        - "PUD_DOWN": pull-down connection
+        - "PUD_UP": pull-up connection 
+    BOTH : bool, optional
+        Enable interruption to both rising and falling edge (default is False)
+    RISING : bool, optional
+        Enable interruption to rising edge (default is False)
+    FALLING : bool, optional
+        Enable interruption to falling edge (default is False)
+    """
+
+    if (pud == 'PUD_DOWN'):
+        GPIO.setup(ls_pin, GPIO.IN, GPIO.PUD_DOWN)
+    elif (pud == 'PUD_UP'):
+        GPIO.setup(ls_pin, GPIO.IN, GPIO.PUD_UP)
+
+    if (BOTH):
+        GPIO.add_event_detect(ls_pin, GPIO.BOTH, int_both, 500)
+    if (RISING):
+        GPIO.add_event_detect(ls_pin, GPIO.RISING, int_rising, 500)
+    if (FALLING):
+        GPIO.add_event_detect(ls_pin, GPIO.FALLING, int_falling, 500)
+    
+    return 
+
+
 class Stepper:
     """
     A class used to group stepper motor settings
@@ -24,15 +58,23 @@ class Stepper:
 
     Attributes
     ----------
-    DIR : GPIO.OUT
-        direction of the stepper motor
+    DIR_PIN : int
+        The RPI's pin to set as direction of the stepper motor.
+        This pin will be connected to the DIR pin in the 
+        Pololu's stepper motor driver (like the A4988).
+    STEP_PIN : int
+        The RPI's pin to set as PWM for steps of the stepper motor.
+        This pin will be connected to the STEP pin in the 
+        Pololu's stepper motor driver (like the A4988).
+    DIR : bool
+        Actual direction of the stepper motor
     STEP : GPIO.PWM
         PWM for steps of the stepper motor
 
-    Methods
-    -------
-    start(duty_cycle=60)
-        stepper motor will start moving
+    Methods 
+    ------- 
+    start(d 
+        ste l start moving
     stop()
         stepper motor will stop moving
     changeDir()
@@ -40,9 +82,8 @@ class Stepper:
     """
 
     def __init__(self, dir, step, vel=25):
-        """Configure a RPi pin to do PWM (Pulse Width Modulation)
-        with GPIO module
-
+        """Configure a RPi pin to do PWM (Pulse Width Modulation) with GPIO module
+        
         Parameters
         ----------
         dir : int
@@ -117,8 +158,9 @@ if __name__ == "__main__":
     GPIO.setup(LED_BLINK, GPIO.OUT, initial=ledstate)
 
     FINC_1 = 12
-    GPIO.setup(FINC_1, GPIO.IN, GPIO.PUD_DOWN)
-    GPIO.add_event_detect(FINC_1, GPIO.BOTH, finc_interrupt, 500)
+    # GPIO.setup(FINC_1, GPIO.IN, GPIO.PUD_DOWN)
+    # GPIO.add_event_detect(FINC_1, GPIO.BOTH, finc_interrupt, 500)
+    limitswitch_setup(FINC_1, 'PUD_DOWN', BOTH=True, int_both=finc_interrupt)
     
 
     # Stepper A setup
@@ -135,4 +177,3 @@ if __name__ == "__main__":
         mA.stop()
         GPIO.cleanup()
         print("\nExiting...")
-
